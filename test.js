8926671ff0c8f856048715bdf56124252a81c412
bilaljanir <bilaljanir@gmail.com>
2022-10-26 11:44:44 +0200

    calcule note

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-console.log ("ça fonctionne");
+
+
+let x =5
+const t= "tutu"
+
+let o = {
+tltl: 6,
+tutu: 38,
+
+}
+
+
+
+let ta = [3,4,5]
+
+
+function average(tableau){
+ let total = 0
+
+ for (const note in tableau)
+ total+=note
+
+return total / tableau.length
+}
+
+console.log(average(ta))
+
+ 
