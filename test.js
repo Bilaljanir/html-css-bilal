013c38531b281bf64691bf98d66cc70de5e3f86c
bilaljanir <bilaljanir@gmail.com>
2022-10-26 12:00:36 +0200

    tets réussi ,raté

--- test.js
+++ test.js
@@ -11,18 +11,30 @@ tutu: 38,
 
 
 
-let ta = [3,4,5]
+let notes_bil=[4,5,6];
+let notes_leo=[4,5,6];
 
 
 function average(tableau){
  let total = 0
 
- for (const note in tableau)
+ for (const note in tableau){
  total+=note
 
 return total / tableau.length
+} }
+
+
+let avg_bil = average(notes_bil)
+let avg_leo = average(notes_leo)
+
+function pass (avg) {
+    if (avg <4){
+        console.log("raté")
+    }else {
+        console.log("reussi")
+    }
 }
 
-console.log(average(ta))
-
- 
+pass(avg_bil)
+pass(avg_leo)
